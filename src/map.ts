f18603e856de1b14a40c530c1a67ada70df7c582
荀涧林 <kalxd@126.com>
2023-08-02 21:02:34 +0800

    遇到技术问题了，做不下去啦！

--- src/map.ts
+++ src/map.ts
@@ -5,11 +5,44 @@ import { EitherAsync } from "purify-ts";
 import { readSetting } from "./internal/storage";
 import MapMain from "./map/main";
 
+const BASE_URL = "https://api.tianditu.gov.cn/api?v=4.0&tk=";
+
+declare  global {
+	interface Window {
+		TMAP_AUTHKEY: string;
+	}
+}
+
+const loadMapResouce = async (token: string): Promise<void> => {
+	const url = `${BASE_URL}${token}`;
+
+	window.TMAP_AUTHKEY = token;
+	const script = document.createElement("script");
+	script.src = "/static/map.js";
+
+	await fetch(url)
+		.then(r => r.text())
+		.then(console.log);
+
+	await new Promise(resolve => {
+		document.body.appendChild(script);
+		script.addEventListener("load", x => {
+			console.log(x);
+			resolve(x);
+		});
+	});
+};
+
 const App = (): m.Component => {
 	const [update, Wait] = waitting();
 
 	update(() => EitherAsync.fromPromise(async () => {
 		const msetting = await readSetting();
+
+		await loadMapResouce("e18ac8e8701747fc4067cff85534f951").catch(e => {
+			console.log(e);
+			return Promise.reject(e);
+		});
 
 		return msetting.toEither("尚未填写token，请到选项中设置！")
 			.map(setting => ({
