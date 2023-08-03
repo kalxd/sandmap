1a6c93f793a469d849ade942e12431d8aaf12b22
荀涧林 <kalxd@126.com>
2023-08-03 21:16:07 +0800

    简单的应用。

--- src/internal/storage.ts
+++ src/internal/storage.ts
@@ -1,1 +1,21 @@
-console.log("he");
+import { string as cstring, Codec, GetType, Maybe } from "purify-ts";
+
+const SETTING_KEY = "setting";
+
+const settiongOption = Codec.interface({
+	token: cstring
+});
+
+export type SettingOption = GetType<typeof settiongOption>;
+
+export const readSetting = (): Maybe<SettingOption> => {
+	const s = localStorage.getItem(SETTING_KEY);
+	return Maybe.fromNullable(s)
+		.map(JSON.parse)
+		.chain(o => settiongOption.decode(o).toMaybe());
+};
+
+export const writeSetting = (setting: SettingOption): void => {
+	const o = JSON.stringify(setting);
+	localStorage.setItem(SETTING_KEY, o);
+};
